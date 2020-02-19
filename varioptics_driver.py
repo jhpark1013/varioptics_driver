bac90c720c1c8f96315c507b7f5ec7e0a411abd0
Jaehee Park <jaehee@ramonaoptics.com>
2020-02-19 15:30:11 -0500

    started varioptics script

---
+++ varioptics_driver.py
@@ -0,0 +1,16 @@
+def _get_port_from_serial_number():
+
+
+class Varioptics:
+
+    def __init__(self, serial_number:str=none):
+        self._serial_number = serial_number
+
+    @staticmethod
+    def list_available_serial_numbers():
+        from serial.tools.list_pors import available_comports
+        available_comports = comports()
+        serial_num_list = []
+        for c in available_comports:
+            serial_num_list.append(c.serial_number)
+        return serial_num_list
