9b4937496be950bf192d26c00dbad0a541b3f398
Jaehee Park <jaehee@ramonaoptics.com>
2020-02-21 15:11:19 -0500

    added baudrate (frequency of data transfer)

--- varioptics_driver.py
+++ varioptics_driver.py
@@ -18,11 +18,18 @@ short_to_bytes = partial(int.to_bytes,
 long_to_bytes = partial(int.to_bytes,
                         length=4, byteorder='little', signed=True)
 class Varioptics:
-
+    _STX = 0x02
+    _ACK = 0x06
+    _NACK = 0x15
+    
     def __init__(self, serial_number:str=None):
         self._serial_number = serial_number
         self._serial = None
         self.test = 11
+        
+        # Needs to be 57600 according to the datasheet
+        self._baudrate = 57600
+
 
     @staticmethod
     def list_available_serial_numbers():
@@ -40,7 +47,7 @@ class Varioptics:
         device = get_port_from_serial_number(serial_number=self._serial_number)
         self._serial_number = device.serial_number
         self._com = device.device
-        self._serial = Serial(port=self._com) #SerialException: could not open port 'COM11': PermissionError(13, 'Access is denied.', None, 5)
+        self._serial = Serial(port=self._com, baudrate=self._baudrate) #SerialException: could not open port 'COM11': PermissionError(13, 'Access is denied.', None, 5)
         #self._serial = Serial()
 
         # try:
@@ -58,11 +65,17 @@ class Varioptics:
     def serial_number(self):
         return self._serial_number
 
-    def _write(self, command, param1=0x00, param2=0x00, param3=0x00, param4=0x00):
-        """test with lens._write(0x0237, 0x03, 0x01, 0xFF, 0x3C)"""
+    def _write(self, command, address=0x00, num_data=0x00, reg_value=0x00, crc=0x00):
+        """test with lens._write(0x37, 0x03, 0x01, 0xFF, 0x3C)"""
         """after writing, read the response to see if transaction is successful"""
-        command = (short_to_bytes(command) +
-                    bytes([param1 & 0xFF, param2 & 0xFF, param3 & 0xFF, param4 & 0xFF]))
+        command = bytes([
+            self._STX,       # Start of transmission
+            command & 0xFF,  # 1 byte command
+            address & 0xFF, 
+            num_data & 0xFF, 
+            reg_value & 0xFF, 
+            crc & 0xFF])
+        import pdb; pdb.set_trace()
         self._serial.write(command)
 
     def _read(self):
